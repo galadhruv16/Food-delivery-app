f0ecf0ab2f21091ca5e2e5405924810f2bad7fdf
Dhruv Gala <galadhruv1611@gmail.com>
2024-06-09 23:31:43 +0530

    added top rated filter

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,33 @@
 import "./App.css";
 import resObj from "../utils/mockAPI";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 function Menu() {
+  const [resData, setResData] = useState(resObj);
   return (
     <div>
       <div className="search">Search</div>
+      <div className="filter-bttn">
+        <button
+          className="top-rated"
+          onClick={() => {
+            const filteredList = resData.filter(
+              (restaurant) => restaurant.info.avgRating > 4.5
+            );
+            console.log(filteredList);
+            setResData(filteredList);
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
       <div className="cardbody">
-        {resObj.map((restaurant) => (
+        {resData.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
   );
 }
-
-
 
 export default Menu;
