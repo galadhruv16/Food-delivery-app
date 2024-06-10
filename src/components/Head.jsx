e881227caac7a44dfedea632bd2556ec1a28d167
Dhruv Gala <galadhruv1611@gmail.com>
2024-06-11 00:49:48 +0530

    added login logout button

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,29 +1,41 @@
 import "./App.css";
 import { LOGO_URL } from "../utils/const";
+import { useState } from "react";
 function Head() {
+  const [login, setLogin] = useState("Login");
   return (
     <div>
       <div className="app">
         <header className="App-header">
           <nav className="navbar">
-            <img
-              className="logo"
-              src= {LOGO_URL}
-              alt="food logo"
-            />
-            <div><h2>DHRUV FOOD DELIVERY APP</h2></div>
+            <img className="logo" src={LOGO_URL} alt="food logo" />
+            <div>
+              <h2>DHRUV FOOD DELIVERY APP</h2>
+            </div>
             <ul className="navbuttons">
               <li>Home</li>
               <li>About us</li>
               <li>Contact us</li>
               <li>Cart</li>
+              <li>
+                <button
+                  className="login"
+                  onClick={() => {
+                    if (login === "Logout") {
+                      setLogin("Login");
+                    } else {
+                      setLogin("Logout");
+                    }
+                  }}
+                >
+                  {login}
+                </button>
+              </li>
             </ul>
           </nav>
         </header>
       </div>
-      
     </div>
-
   );
 }
 
