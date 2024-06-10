66853efa72ebafe5601f9f346bf18d0ac1d10d27
Dhruv Gala <galadhruv1611@gmail.com>
2024-06-11 00:22:23 +0530

    added live api and shimmer ui

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
-import resObj from "../utils/mockAPI";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import Shimmer from "./Shimmer.jsx";
+import { useState, useEffect } from "react";
 function Menu() {
-  const [resData, setResData] = useState(resObj);
+  const [resData, setResData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const res = await data.json();
+
+    setResData(
+      res.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  if (resData.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div>
       <div className="search">Search</div>
---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,42 @@
+
+
+function Shimmer() {
+  return (
+      <div>
+        <div>Search</div>
+        <div><button>Top Rated</button></div>
+      <div className="shimmer-cont">
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+      <div className="shimbox"><div className="img"></div></div>
+    </div>
+    </div>
+  )
+}
+
+export default Shimmer
