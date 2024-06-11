45aecc7c923e7ea7b11d23744e987ed725598157
Dhruv Gala <galadhruv1611@gmail.com>
2024-06-11 08:58:34 +0530

    changed css

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -22,7 +22,7 @@ function Menu() {
     return <Shimmer />;
   }
   return (
-    <div>
+    <div className="cardbg">
       <div className="search">
         <input
           type="text"
@@ -40,11 +40,20 @@ function Menu() {
             setResData(filteredList);
           }}
         >
-          Search
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+          </svg>
         </button>
       </div>
       <div className="filter-bttn">
-        <button
+        <button 
           className="top-rated"
           onClick={() => {
             const filteredList = resData.filter(
